3e275245a14dc5bc60ff1053aadbfa3f586a84aa
Aoyia <Aoyia@2961617189@qq.com>
2020-09-15 16:42:26 +0800

    placeholder转移到js，由js插入自己要操作的代码，实现图片库的结构、样式和行为彻底分离

--- script/showPic.js
+++ script/showPic.js
@@ -1,31 +1,3 @@
-function showPic(whichpic) {
-	if (!document.getElementById("placeholder")) return false;
-	var source = whichpic.getAttribute("href");
-	var placeholder = document.getElementById("placeholder");
-	if (placeholder.nodeName != "IMG") return false;
-	placeholder.setAttribute("src", source);
-	if (document.getElementById("description")) {
-		var text = whichpic.getAttribute("title") ? whichpic.getAttribute("title") : " ";
-		var description = document.getElementById("description");
-		console.log(text);
-		if (description.firstChild.nodeType == 3) {
-			description.firstChild.nodeValue = text;
-			console.log(description.firstChild.nodeValue);
-		}
-
-	}
-	return true;
-}
-
-function prepareGallery() {
-	var gallery = document.getElementById("imagegallery");
-	var links = gallery.getElementsByTagName("a");
-	for (let i = 0; i < links.length; i++) {
-		links[i].onclick = function () {
-			return !showPic(this);
-		}
-	}
-}
 // 迷之代码
 function addLoadEvent(func) {
 	var oldonload = window.onload;
@@ -38,5 +10,55 @@ function addLoadEvent(func) {
 		}
 	}
 }
+function insertAfter(newElement, targetElement) {
+	var parent = targetElement.parentNode;
+	if (parent.lastChild == targetElement) {
+		parent.appendChild(newElement);
+	} else {
+		parent.insertBefore(newElement, targetElement.nextSibling);
+	}
+}
+function preparePlaceholder() {
+	if (!document.getElementById("imagegallery")) {
+		return false;
+	}
+	var placeholder = document.createElement("img");
+	placeholder.setAttribute("id", "placeholder");
+	placeholder.setAttribute("src", "images/5.jpg");
+	placeholder.setAttribute("alt", "这是一个图片");
+	var description = document.createElement("p");
+	description.setAttribute("id", "description");
+	var desctext = document.createTextNode("这是一张美图");
+	description.appendChild(desctext);
+	var targetElement = document.getElementById("imagegallery");
+	insertAfter(placeholder, targetElement);
+	insertAfter(description, placeholder);
+}
+function showPic(whichpic) {
+	var source = whichpic.getAttribute("href");
+	var placeholder = document.getElementById("placeholder");
+	placeholder.setAttribute("src", source);
+	if (document.getElementById("description")) {
+		var text = whichpic.getAttribute("title") ? whichpic.getAttribute("title") : " ";
+		var description = document.getElementById("description");
+		console.log(text);
+		if (description.firstChild.nodeType == 3) {
+			description.firstChild.nodeValue = text;
+			console.log(description.firstChild.nodeValue);
+		}
+	}
+	return false;
+}
 
+function prepareGallery() {
+	var gallery = document.getElementById("imagegallery");
+	var links = gallery.getElementsByTagName("a");
+	for (var i = 0; i < links.length; i++) {
+		links[i].onclick = function () {
+			return showPic(this);
+		}
+	}
+}
+addLoadEvent(preparePlaceholder);
 addLoadEvent(prepareGallery);
+
